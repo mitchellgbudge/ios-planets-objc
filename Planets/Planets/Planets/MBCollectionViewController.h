ecf4c24edddc825d08a873aabab3a228adebfbc7
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-04 14:16:00 -0700

    Configure collection view and accompanying files

---
+++ Planets/Planets/Planets/MBCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  MBCollectionViewController.h
+//  Planets
+//
+//  Created by Mitchell Budge on 11/4/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
