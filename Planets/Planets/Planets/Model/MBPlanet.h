cc797469c9d930113e6e921f3bed290531a7fc1f
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-04 14:57:34 -0700

    Establish model object

---
+++ Planets/Planets/Planets/Model/MBPlanet.h
@@ -0,0 +1,17 @@
+//
+//  MBPlanet.h
+//  Planets
+//
+//  Created by Mitchell Budge on 11/4/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBPlanet : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Planets/Planets/Planets/Model/MBPlanet.m
@@ -0,0 +1,13 @@
+//
+//  MBPlanet.m
+//  Planets
+//
+//  Created by Mitchell Budge on 11/4/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import "MBPlanet.h"
+
+@implementation MBPlanet
+
+@end
